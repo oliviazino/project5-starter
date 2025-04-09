95f4217c489ab5816595ddcfbe769628fea154d2
oliviazino <ozino@nd.edu>
2025-04-08 20:15:38 -0400

    replacement algs iteration 1

--- main.c
+++ main.c
@@ -35,6 +35,15 @@ int *frame_table;
 /* global variable to identify the algorithm we should use -- given to us in main() */
 const char *algoname;
 
+/* hand of the clock */
+int hand = 0; 
+
+/* for clock to compare to */
+int* refSeq;
+
+/* to be used in custom alg */
+int *frequency;
+
 /* find an available frame using a selected replacement alg */
 int freeFrameFinder(struct page_table *pt, int page, int *kicked) { 
 	// is there a frame currently free to fill? 
@@ -49,18 +58,67 @@ int freeFrameFinder(struct page_table *pt, int page, int *kicked) {
 		int replacedFrame = rand() % page_table_get_nframes(pt); // make sure value is an actual frame 
 		*kicked = frame_table[replacedFrame];
 		return replacedFrame; 
+
     } else if (!strcmp(algoname, "clock")) {
         // CLOCK logic from class 
+		int nframes = page_table_get_nframes(pt);
+		while (1) { 
+			if (refSeq[hand] == 0) { 
+				// kick this frame
+				int replacedFrame = hand; 
+				*kicked = frame_table[replacedFrame];
+				hand = (hand + 1) % nframes; 
+				return replacedFrame;
+			} else { 
+				// check again just in case 
+				refSeq[hand] = 0; 
+				hand = (hand + 1) % nframes;
+			}
+		}
+
     } else if (!strcmp(algoname, "custom")) {
-        // what we have come up with
+        // what we have come up with = using frequency as a tiebreaker for CLOCK
+		int nframes = page_table_get_nframes(pt);
+    int start = hand;
+    int candidate = -1;
+    int lowestFreq = __INT_MAX__;
+
+    while (1) {
+        if (refSeq[hand] == 0) {
+            int page = frame_table[hand];
+            if (frequency[hand] < lowestFreq) {
+                candidate = hand;
+                lowestFreq = frequency[hand];
+            }
+        } else {
+            refSeq[hand] = 0;
+        }
+        hand = (hand + 1) % nframes;
+       // we have returned back to 00:00 
+        if (hand == start) {
+            break;
+        }
     }
+    if (candidate != -1) {
+        *kicked = frame_table[candidate];
+        hand = (candidate + 1) % nframes;
+        return candidate;
+    }
+
+    // if all had 1 --> just use hand
+    *kicked = frame_table[hand];
+    int victim = hand;
+    hand = (hand + 1) % nframes;
+    return victim;
+	}
 }
 /* A dummy page fault handler to start.  This is where most of your work goes. */
 void page_fault_handler(struct page_table *pt, int page )
 {
 	/* // try it out portion -- page N maps directly to frame N: 
 	page_table_set_entry(pt, page, page, BIT_PRESENT | BIT_WRITE); */
-	int frame = find_frame(pt, page, &kicked);
+	int kicked = -1;
+	int frame = freeFrameFinder(pt, page, &kicked);
 	page_table_set_entry(pt, page, frame, BIT_PRESENT);
 	/* original dummy code
 	printf("page fault on page #%d\n",page);
@@ -71,7 +129,9 @@ void page_fault_handler(struct page_table *pt, int page )
 	//int bits; 
 	//page_table_get_entry(pt, page, &frame, &bits);
 
+	/* CASE 1 -- PAGE IS PRESENT - WRITE FAULT OCCURS */
 
+	/* CASE 2 -- PAGE IS NOT PRESENT AND WE NEED TO FIND A FRAME */
 }
 
 int main( int argc, char *argv[] )
@@ -83,7 +143,7 @@ int main( int argc, char *argv[] )
 
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
-	const char *algoname = argv[3];
+	algoname = argv[3];
 	const char *program = argv[4];
 
 	disk = disk_open("myvirtualdisk",npages);
@@ -100,6 +160,17 @@ int main( int argc, char *argv[] )
 
 	physmem = page_table_get_physmem(pt);
 	virtmem = page_table_get_virtmem(pt);
+
+	/* data structures for replacement algs */
+	frame_table = malloc(sizeof(int) * nframes);
+	refSeq = malloc(sizeof(int) * nframes);
+	frequency = malloc(sizeof(int) * nframes);
+
+	for (int i = 0; i < nframes; i++) {
+		frame_table[i] = -1;
+		refSeq[i] = 0;
+		frequency[i] = 0;
+	}
 	
 	if(!strcmp(program,"alpha")) {
 		alpha_program(pt,virtmem,npages*PAGE_SIZE);
